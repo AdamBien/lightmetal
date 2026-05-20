3a3ef5a9c7b311d53a66e8839315bfd5189c8543
Adam Bien <abien@adam-bien.com>
2026-05-20 06:45:18 +0200

    refactored to an interface

--- src/main/java/lm/prompting/control/PromptTemplate.java
+++ src/main/java/lm/prompting/control/PromptTemplate.java
@@ -1,8 +1,6 @@
 package lm.prompting.control;
 
-public final class PromptTemplate {
-
-    private PromptTemplate() {}
+public interface PromptTemplate {
 
     public static String mistralInstruct(String userPrompt) {
         return "[INST] " + userPrompt + " [/INST]";
